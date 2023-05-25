f2fc22dc2a6193377f955a03b6950593ede9b32d
gabbyBliss <133480623+gabbyBliss@users.noreply.github.com>
2023-05-25 08:22:12 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Gabriel Blessing Nnenna";
+$regnumber = "2021/HND/33493/CS";
+$soo = "Ebonyi State";
+$topic_of_project = "Online Computer aided Troubleshooting in Automobile";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
